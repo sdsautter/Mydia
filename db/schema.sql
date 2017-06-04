bbb66f3fcd2f481d31522f5e7f65b3188e34c955
sdsautter <sdsautter@gmail.com>
2017-06-04 11:14:26 -0400

    Added skeleton and made it so a simple page shows with pug

---
+++ db/schema.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE mydia_db;
---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // index route loads view.html
+    // app.get("/", function(req, res) {
+    //   res.sendFile(path.join(__dirname + "/../views/layouts/main.handlebars"));
+    // });
+
+    app.get('/', function(req, res) {
+        res.render('index', { title: 'Hey', message: 'Hello there!' })
+    })
+
+};
