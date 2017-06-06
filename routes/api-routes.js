a80bdcc06a42ab7ee42473eac47c044681f163f5
Byron Anderson <Byron Anderson>
2017-06-05 21:35:06 -0400

    sequelized added

---
+++ routes/api-routes.js
@@ -0,0 +1,44 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    app.get('/:user', function(req, res) {
+        db.User.findAll({
+            where: {
+                id: req.body.id
+            }
+        }).then(function(userData) {
+            res.render('user', userData)
+        });
+    }),
+
+    app.get('/movies/:id', function(req, res) {
+        db.Movie.findAll({
+            where: {
+                id: req.body.id
+            }
+        }).then(function(movieData) {
+            res.render('movie', movieData)
+        });
+    }),
+
+    app.get('/music/:id', function(req, res) {
+        db.Music.findAll({
+            where: {
+                name: req.body.name
+            }
+        }).then(function(musicData) {
+            res.render('music', musicData)
+        });
+    })
+
+};
