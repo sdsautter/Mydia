4e07c5a80a5ac29b159fda16a15c32d56dedb24b
sdsautter <sdsautter@gmail.com>
2017-06-08 18:59:10 -0400

    added google books and made the get call its own function

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -4,9 +4,9 @@ $("#lookie").on("click", function(event) {
     event.preventDefault();
 
     var mediaSearch = $("#search").val().trim();
-    var replaced = mediaSearch.replace(/\s/g, '+');
+    var mediaSearch = mediaSearch.replace(/\s/g, '+');
 
-    movieSearch(replaced);
+    movieSearch(mediaSearch);
 
 });
 
@@ -14,12 +14,22 @@ $('.message a').click(function() {
     $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
 });
 
+//API call for OMDB
 function movieSearch(movie) {
-    
     var query = "http://www.omdbapi.com/?apikey=" + omdbKey + "&s=" + movie + "&r=json";
+    getSearch(query);
+}
+
+//API call for google books
+function bookSearch(book) {
+    var query = "https://www.googleapis.com/books/v1/volumes?q=" + book;
+    getSearch(query);
+}
+
+//Does the api get calls for whatever query we put into it
+function getSearch(query) {
     $.get(query, function(data) {
         console.log(data);
         return data;
     });
-
 }
