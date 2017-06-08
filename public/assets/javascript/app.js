ce67db365f4fe6e72bd17edc7e8426124f657a04
sdsautter <sdsautter@gmail.com>
2017-06-08 18:13:07 -0400

    Made the omdb call it's own function

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,20 +1,25 @@
 var omdbKey = OmdbConfig.apiKey;
 
-$("#lookie").on("click", function(event){
+$("#lookie").on("click", function(event) {
     event.preventDefault();
 
-    var movie = $("#search").val().trim();
-    var replaced = movie.replace(/\s/g, '+');
-    var query = "http://www.omdbapi.com/?apikey=" + omdbKey + "&s=" + movie + "&r=json";
+    var mediaSearch = $("#search").val().trim();
+    var replaced = mediaSearch.replace(/\s/g, '+');
 
+    movieSearch(replaced);
+
+});
+
+$('.message a').click(function() {
+    $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
+
+function movieSearch(movie) {
+    
+    var query = "http://www.omdbapi.com/?apikey=" + omdbKey + "&s=" + movie + "&r=json";
     $.get(query, function(data) {
-    	console.log(data);
+        console.log(data);
+        return data;
     });
 
-    // console.log(query);
-});
-
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
-
+}
