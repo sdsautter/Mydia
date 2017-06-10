d6ef5951a68f82768def00e39eaf333835724b02
sdsautter <sdsautter@gmail.com>
2017-06-10 16:34:16 -0400

    Fixed url

--- controllers/last_fm_controller.js
+++ controllers/last_fm_controller.js
@@ -7,15 +7,10 @@ module.exports = function(app) {
     // GET route for getting all of the Burgers
     app.get("/search/album/:title", function(req, res) {
         var albumTitle = req.params.title;
-        request(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${albumTitle}&limit=30&api_key=$${LastFmKey}&format=json`, function(error, response, body) {
+        request(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${albumTitle}&limit=30&api_key=${LastFmKey}&format=json`, function(error, response, body) {
             // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
-                // console.log(movieName);
-
-                // console.log(body.totalResults);
-                console.log(JSON.parse(body));
-                // console.log(JSON.parse(body));
-
+                console.log(JSON.parse(body).results.albummatches);
 
                 var album = {
                     album: JSON.parse(body).results.albummatches
