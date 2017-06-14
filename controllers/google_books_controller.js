578acd0f9a9c387394e59690df14514a51fffb61
sdsautter <sdsautter@gmail.com>
2017-06-14 10:21:55 -0400

    Adds new_description with double quotes being replaced

--- controllers/google_books_controller.js
+++ controllers/google_books_controller.js
@@ -1,16 +1,32 @@
 var request = require("request");
 
-module.exports = function(app) {
+var bookArray = [];
+module.exports = function (app) {
     // GET route for getting all of the Burgers
-    app.get("/search/book/:title", function(req, res) {
+    app.get("/search/book/:title", function (req, res) {
+        bookArray = [];
         var bookTitle = req.params.title;
-        request(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&maxResults=30`, function(error, response, body) {
+        request(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&maxResults=30`, function (error, response, body) {
 
             // If the request is successful (i.e. if the response status code is 200)
             if (!error && response.statusCode === 200) {
-                var book = {
-                    book: JSON.parse(body).items
+                // console.log(JSON.parse(body).items.length);
+                for (var i = 0; i < JSON.parse(body).items.length; i++) {
+                    var bookPath = JSON.parse(body).items[i];
+                    if (bookPath.volumeInfo.description) {
+                        var description = JSON.parse(body).items[i].volumeInfo.description;
+                        var replaceQuote = description.replace(/"/g, "'");
+                        bookPath.new_description = replaceQuote;
+                        bookArray.push(bookPath);
+                    } else {
+                        bookArray.push(bookPath);
+                    }
                 }
+                
+                var book = {
+                    book: bookArray
+                }
+
                 res.render("index", book);
             }
         });
