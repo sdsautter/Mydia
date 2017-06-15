145eb14857a0b8792dfeec0277b723701d1a74eb
sdsautter <sdsautter@gmail.com>
2017-06-15 18:10:09 -0400

    heroku

---
+++ config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    Omdb: {
+        apiKey: "b6dc494b"
+    },
+
+    LastFm: {
+        applicationName: "Mydia",
+        apiKey: "ab523235bd0abe0d63f1867cd77e775f",
+        sharedSecret: "5b1da75e08c1ca1d6d2c181009485bfc"
+    },
+
+    GiantBomb: {
+        apiKey: "a15a77dc3f2b8c5b254fad9cd131dc8e44121dc7"
+    },
+    
+    ComicVine: {
+        apiKey: "1ba825af687605559460750f37959c408524c16e"
+    }
+}
